d2be9737a769224dd01f7d7249b8752bace6984f
Jarred Capellman <jcapellman@hotmail.com>
2020-10-23 18:37:04 -0500

    Added: Initial work

---
+++ METLParser.cs
@@ -0,0 +1,21 @@
+﻿using METL.Enums;
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace METL
+{
+    public class METLParser
+    {
+        public byte[] EmbedFromBytes([NotNull]byte[] source, [NotNull]byte[] embedBytes, GenerationOption generationOption = GenerationOption.APPEND)
+        {
+            switch (generationOption)
+            {
+                case GenerationOption.APPEND:
+                    System.Buffer.BlockCopy(embedBytes, 0, source, 0, source.Length);
+                    break;
+            }
+
+            return embedBytes;
+        }
+    }
+}
