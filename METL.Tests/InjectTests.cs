f132cc0f652d2420b5205c62a6c0f4a057d0d9d7
Jarred Capellman <jcapellman@hotmail.com>
2020-10-23 20:33:03 -0500

    Added: Initial work on the InjectTests

---
+++ METL.Tests/InjectTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace METL.Tests
+{
+    [TestClass]
+    public class InjectTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullCode()
+        {
+            METLParser.InjectMalwareFromFile(null, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void CodeNotFound()
+        {
+            METLParser.InjectMalwareFromFile(Path.GetRandomFileName(), Path.GetRandomFileName());
+        }
+    }
+}
