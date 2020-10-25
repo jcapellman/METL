30d55eb92fa699362c8d444c44b8c5ec5f74e0b6
Jarred Capellman <jcapellman@hotmail.com>
2020-10-25 15:37:09 -0500

    Added: Initial work on the Timestamp class

---
+++ METL/InjectorMerges/Timestamp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using METL.InjectorMerges.Base;
+
+namespace METL.InjectorMerges
+{
+    public class Timestamp : BaseInjectorMerge
+    {
+        public override string FIELD_NAME => "TIMESTAMP";
+
+        public override string Merge() => DateTime.Now.ToString();
+    }
+}
