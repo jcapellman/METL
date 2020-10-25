e53befd484af44dcde3140982a9599a5f92f0a53
Jarred Capellman <jcapellman@hotmail.com>
2020-10-25 15:35:18 -0500

    Added: BaseInjectorMerge abstract class

---
+++ METL/InjectorMerges/Base/BaseInjectorMerge.cs
@@ -0,0 +1,9 @@
+﻿namespace METL.InjectorMerges.Base
+{
+    public abstract class BaseInjectorMerge
+    {
+        public abstract string FIELD_NAME { get; }
+
+        public abstract string Merge();
+    }
+}
